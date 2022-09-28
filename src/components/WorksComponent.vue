112f0be80b50eebac0c4b9082ae5eeac494cc2ff
新田一斗 <shintaichito@dot1xs1861.14-192-19.wlan.ms.dendai.ac.jp>
2022-09-28 15:16:32 +0900

    リストで渡すことでも表示できるように

---
+++ src/components/WorksComponent.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "WorkModule",
+  props: ["works"],
+};
+</script>
+
+<template>
+  <div v-for="work in works" :key="work.id" class="card work">
+    <div class="box">
+      <router-link :to="'/works/' + work.id">
+        <img :src="work.image_path" />
+        <div class="card_content">
+          <div class="desc">{{ work.short_description }}</div>
+        </div>
+      </router-link>
+      <div class="title">{{ work.title }}</div>
+    </div>
+  </div>
+</template>
--- src/views/WorksView.vue
+++ src/views/WorksView.vue
@@ -1,24 +1,14 @@
 <template>
   <main class="container">
   <div class="works">
-    <WorkComponent :id="work.id" :title="work.title" :image_path="work.image_path" :short_description="work.short_description" />
-    <WorkComponent :id="work2.id" :title="work2.title" :image_path="work2.image_path" :short_description="work2.short_description" />
-    <WorkComponent :id="test.id" :title="test.title" :image_path="test.image_path" :short_description="test.short_description" />
-    <WorkComponent :id="test.id" :title="test.title" :image_path="test.image_path" :short_description="test.short_description" />
-    <WorkComponent :id="work.id" :title="work.title" :image_path="work.image_path" :short_description="work.short_description" />
-    <WorkComponent :id="work2.id" :title="work2.title" :image_path="work2.image_path" :short_description="work2.short_description" />
-    <WorkComponent :id="test.id" :title="test.title" :image_path="test.image_path" :short_description="test.short_description" />
-    <WorkComponent :id="test.id" :title="test.title" :image_path="test.image_path" :short_description="test.short_description" />
-    <WorkComponent :id="work.id" :title="work.title" :image_path="work.image_path" :short_description="work.short_description" />
-    <WorkComponent :id="work2.id" :title="work2.title" :image_path="work2.image_path" :short_description="work2.short_description" />
-    <WorkComponent :id="test.id" :title="test.title" :image_path="test.image_path" :short_description="test.short_description" />
-    <WorkComponent :id="test.id" :title="test.title" :image_path="test.image_path" :short_description="test.short_description" />
+    <WorksComponent :works="works" />
   </div>
   </main>
 </template>
 
 <script>
-import WorkComponent from '@/components/WorkComponent.vue';
+import WorksComponent from '@/components/WorksComponent.vue';
+
 import test from '../../public/works/0.json';
 import work from '../../public/works/1.json';
 import work2 from '../../public/works/2.json';
@@ -26,16 +16,13 @@ import work2 from '../../public/works/2.json';
 import "../../public/css/works.css";
 
 export default {
-  name: 'App',
   data() {
     return {
-      test: test,
-      work: work,
-      work2: work2
+      works: [work, work2, test]
     };
   },
   components: {
-    WorkComponent
+    WorksComponent
   },
 };
 </script>
