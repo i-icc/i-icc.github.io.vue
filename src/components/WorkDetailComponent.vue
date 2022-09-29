d25ab2e82c3f03402d1d5a188434eb7dfaeda65a
新田一斗 <shintaichito@shintaichitonoMacBook-Air.local>
2022-09-29 19:25:26 +0900

    詳細画面

--- src/components/WorkDetailComponent.vue
+++ src/components/WorkDetailComponent.vue
@@ -6,17 +6,27 @@ export default {
 
 <template>
   <div class="border-bottom mt-5 mb-5">
-    <h1 class="display-2">{{ work.title }}</h1>
-    <div class="display-10 mb-4">
+    <h1 class="display-1">{{ work.title }}</h1>
+    <div class="display-8 mb-2">
       {{ work.short_description }}
     </div>
-    <div>{{ work.tag }}</div>
+    <div class="tags mb-2">
+      <span
+        class="badge rounded-pill bg-light text-dark tag"
+        v-for="t in work.tag"
+        :key="t"
+        >{{ t }}</span
+      >
+    </div>
   </div>
 
   <div class="row">
-    <div class="col-md-8 mb-5">
-      {{ work.description }}
+    <div class="col-md-7 mb-3">
+      <div v-for="d in work.description" :key="d">
+        <p>{{ d }}</p>
+      </div>
     </div>
+    <div class="col-md-1"></div>
     <div class="col-md-4">
       <div class="movie-wrap mb-3" v-if="work.media">
         <iframe
@@ -30,11 +40,33 @@ export default {
         ></iframe>
       </div>
       <div class="mb-4">
-        <a :href="work.link" target="_blank">サービス</a>
+        <div class="row links">
+          <div class="col-md-2"></div>
+          <div class="col-md-2 mb-2">
+            <form :action="work.git" target="_blank" v-if="work.git">
+              <input type="image" src="/img/git_logo.png" alt="github" />
+            </form>
+          </div>
+          <div class="col-md-8">
+            <form :action="work.link" target="_blank" v-if="work.link">
+              <button>サービスへ</button>
+            </form>
+          </div>
+        </div>
       </div>
-      <div>
-        <div class="border-bottom display-10 mb-2">使用技術</div>
-        <div>{{ work.tech }}</div>
+      <div class="border-bottom display-6 mb-2">使用技術</div>
+      <div class="tags">
+        <span
+          class="badge rounded-pill bg-light text-dark tag"
+          v-for="t in work.tech"
+          :key="t"
+          >{{ t }}</span
+        >
+      </div>
+      <div class="mt-4 center-block">
+        <div id="pre-page">
+          <router-link to="/works"><i class="fas fa-angle-left"></i></router-link>
+        </div>
       </div>
     </div>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
   </div>
+  <router-link class="dropdown-item" to="/works">All</router-link>
 </template>
 
 <script>
