db937a3219a3d83ab5c684a9d3fc84bdc44f0206
新田一斗 <shintaichito@shintaichitonoMacBook-Air.local>
2022-10-02 12:40:01 +0900

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,8 @@
     <div class="m-5 home">
       <div class="display-1">空想倉庫(仮)</div>
       <div class="display-4 mb-2">
-        説明たくさんするぞいぞい。<br />説明たくさんするぞいぞい<br />説明たくさんするぞいぞい<br />説明たくさんするぞいぞい
+        思いついたものを徒然なるまま作っています。<br>
+        半年に一度くらいのペースで動画も出します。
       </div>
     </div>
 
@@ -15,13 +16,13 @@
 
     <div class="row mb-5">
       <div class="col-md-3 display-6 link" v-for="link in links" :key="link.l">
-        <a class="nav-link" :href="link.l" style="color: black" target="_blank">{{
-          link.t
-        }}</a>
+        <a class="nav-link" :href="link.l" style="color: black" target="_blank"
+          ><img :src="link.i"
+        /></a>
       </div>
     </div>
     <div class="row">
-      <div class="col-md-6 display-5">技術スタックとか所属や生い立ち等</div>
+      <div class="col-md-6 display-5">技術スタックとか近いうちにここに書かれます。</div>
     </div>
   </main>
 </template>
@@ -37,11 +38,19 @@ export default {
         {
           l: "https://www.youtube.com/channel/UCJPHtSJNPvXaMm-DO2Hu8_w",
           t: "youtube",
-          i: "",
+          i: "/img/youtube_logo.png",
         },
-        { l: "https://www.nicovideo.jp/user/96299447", t: "niconico", i: "" },
+        {
+          l: "https://www.nicovideo.jp/user/96299447",
+          t: "niconico",
+          i: "/img/niconico_logo.png",
+        },
         { l: "https://github.com/i-icc", t: "git", i: "/img/git_logo.png" },
-        { l: "https://twitter.com/shark_engineer", t: "twitter", i: "" },
+        {
+          l: "https://twitter.com/shark_engineer",
+          t: "twitter",
+          i: "/img/twitter_logo.png",
+        },
       ],
     };
   },
